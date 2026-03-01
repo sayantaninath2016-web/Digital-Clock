d1fbff4793e22d079362b0330400ce38cfb5f30f
sayantaninath2016-web <sayantaninath2016@gmail.com>
2026-03-01 14:53:54 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-function showTime(){
-    setInterval(()=>{
-        let d=new Date ()
-        let msg=document.getElementById("msg")
-        let rest=""
-        if (d.getHours()>12){
-            rest=""
-        }
-        else if (d.getHours()<12){
-            rest=""
-        }
-        let dateStructure=d.getHours()+":"+d.getMinutes()+":"+d.getSeconds()+rest
-        msg.innerHTML=dateStructure
-    },1000)
-}
+console.log("Welcome though")
+document.writeln("Creator of the page: Sayantani Nath")
+document.writeln("<br>Age: 29")
+document.writeln("<br>Qualification: MA in English")
+document.writeln("<br>Experience: Content Writer")
+let Name=" Sayantani Nath" 
+var Age="29"
+document.writeln("<br>Name:"+Name+"<br>")
+document.writeln("Age:"+Age+"<br>")
+
+    var a="10"
+    var b="15"
+    var c="c"
+
+console.log(a)
+document.writeln("<br>Output1:"+(a+b)+"<br>")
+document.writeln("<br>Output2:"+(a-b)+"<br>")
+document.writeln("<br>Output3:"+(a*b)+"<br>")
+document.writeln("<br>Output4:"+(a/b)+"<br>")
+console.log(typeof(10))
+console.log(typeof(c))
+console.log( 45 > 5 )
+console.log( 45 < 5 )
+console.log( 45 == 45 )
+console.log( (-45 > 0) ?"Positive":"Negative" )
+console.log( (44 % 2 == 0) ?"even":"odd" )
